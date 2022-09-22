f7e2a6dde5af9038b52cedabb4a171c4ec67266c
Pushkar Tatiya <pushkart2@gmail.com>
2022-09-22 11:13:29 -0500

    spawning of mic prop

--- client/main.lua
+++ client/main.lua
@@ -1,4 +1,7 @@
 local oldProximity = 0.0
+
+local prop = "v_ilev_fos_mic"
+
 function string.starts(String,Start)    
     return string.sub(String,1,string.len(Start))==Start 
 end
@@ -8,9 +11,41 @@ CreateThread(function()
     end
 end)
 
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(7)
+        inRange = false
+        local ped = PlayerPedId()
+        local pos = GetEntityCoords(ped)
+        for k,v in ipairs(Config.MicrophoneZones) do
+            local dist = #(pos - v.coords)
+            if dist <= 150.0 then
+                if v.obj == nil then
+                    local obj = CreateObject(GetHashKey(prop), vector3(v.coords.x, v.coords.y, v.coords.z - 1.0), false)
+                    if v.data.heading ~= nil then
+                        SetEntityHeading(obj, v.heading)
+                    end
+                    FreezeEntityPosition(obj, true)
+                    v.obj = obj
+                end
+            else
+                if v.obj then
+                    DeleteEntity(v.obj)
+                    v.obj = nil
+                end
+            end
+        end
+
+		if not inRange then
+			Citizen.Wait(500)
+		end
+	end
+end)
 RegisterNetEvent("ps-zones:enter", function(ZoneName, ZoneData)
     -- Code here
+    
     if string.starts(ZoneName, "microphone_") then
+        print(json.encode(ZoneData))
         oldProximity =  LocalPlayer.state['proximity'].distance
         exports["pma-voice"]:overrideProximityRange(50.0, true)
     end
@@ -21,5 +56,15 @@ RegisterNetEvent("ps-zones:leave", function(ZoneName, ZoneData)
     if string.starts(ZoneName, "microphone_") then
         print("exited_microphone_zone")
         exports["pma-voice"]:clearProximityOverride()
+    end
+end)
+
+AddEventHandler('onResourceStop', function(resource)
+	if (GetCurrentResourceName() ~= resource) then return end
+    for k, v in pairs(Config.MicrophoneZones) do
+        if v.obj then
+            DeleteEntity(v.obj)
+            v.obj = nil
+        end
     end
 end)
