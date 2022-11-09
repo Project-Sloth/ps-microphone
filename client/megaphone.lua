8b9747c8d1aaa06c4032186dcfb2184ad675dc88
Pushkar Tatiya <pushkart2@gmail.com>
2022-11-09 10:08:53 -0600

    remove dpemote dependency

--- client/megaphone.lua
+++ client/megaphone.lua
@@ -1,33 +1,57 @@
 local holdingMega = false
+local prop = nil
 
-RegisterNetEvent("animations:client:EmoteCommandStart", function(table)
-    if table[1] == "c" and holdingMega then
-        holdingMega = false
-        exports["pma-voice"]:clearProximityOverride()
+local function loadAnimDict(dict)
+    while (not HasAnimDictLoaded(dict)) do
+        RequestAnimDict(dict)
+        Citizen.Wait(1)
     end
+end
+
+local function LoadPropDict(model)
+    while not HasModelLoaded(GetHashKey(model)) do
+        RequestModel(GetHashKey(model))
+        Wait(10)
+    end
+end
+function AddPropToPlayerAndAnim(prop1, bone, off1, off2, off3, rot1, rot2, rot3)
+    loadAnimDict("amb@world_human_mobile_film_shocking@female@base")
+    local Player = PlayerPedId()
+    local x,y,z = table.unpack(GetEntityCoords(Player))
+    if not HasModelLoaded(prop1) then
+        LoadPropDict(prop1)
+    end
+    prop = CreateObject(GetHashKey(prop1), x, y, z+0.2,  true,  true, true)
+    AttachEntityToEntity(prop, Player, GetPedBoneIndex(Player, bone), off1, off2, off3, rot1, rot2, rot3, true, true, false, true, 1, true)
+    SetModelAsNoLongerNeeded(prop1)
+    TaskPlayAnim(Player, "amb@world_human_mobile_film_shocking@female@base", "base", 8.0, 1.0, -1, 49, 0, 0, 0, 0)
+
+end
+
+RegisterCommand("mega", function()
+    TriggerEvent("megaphone:Toggle")
 end)
 
 RegisterNetEvent("megaphone:Toggle", function()
-    print(holdingMega)
     if not holdingMega then
         holdingMega = true
         CreateThread(function()
-            while true do
+            while holdingMega do
                 Wait(1000) 
                 if not IsEntityPlayingAnim(PlayerPedId(),"amb@world_human_mobile_film_shocking@female@base", "base", 3) and holdingMega then
                     holdingMega = false
-                    TriggerEvent('animations:client:EmoteCommandStart', {"c"})
+                    ClearPedTasksImmediately(PlayerPedId())
                     exports["pma-voice"]:clearProximityOverride()
-                    TerminateThisThread()
-                    break
+                    DeleteEntity(prop)
                 end
             end
         end)
-        TriggerEvent('animations:client:EmoteCommandStart', {"megaphone"})
+        AddPropToPlayerAndAnim("prop_megaphone_01", 28422, 0.0, 0.0, 0.0, 0.0, 0.0, 80.0)
         exports["pma-voice"]:overrideProximityRange(50.0, true)
     else
         holdingMega = false
-        TriggerEvent('animations:client:EmoteCommandStart', {"c"})
+        ClearPedTasksImmediately(PlayerPedId())
+        DeleteEntity(prop)
         exports["pma-voice"]:clearProximityOverride()
     end
 end)
