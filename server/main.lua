1f96fbe5593dab40ad29fe1339495559c8ebfe61
Chaosfelix4451 <46029764+Chaosfelix4451@users.noreply.github.com>
2022-12-08 15:19:41 +0100

    Updated Serversided main.lua to fit qb-inventory
    Just added the qb core function to actually fit the default qb system a auto detection also could be added with this but i have no time for this at the moment :/

--- server/main.lua
+++ server/main.lua
@@ -1,3 +1,9 @@
-exports['lj-inventory']:CreateUsableItem("megaphone", function(source,item)
+--exports['lj-inventory']:CreateUsableItem("megaphone", function(source,item)
+--    TriggerClientEvent("megaphone:Toggle",source)
+--end)
+local QBCore = exports['qb-core']:GetCoreObject()
+QBCore.Functions.CreateUseableItem('megaphone', function(source, item)
+	local Player = QBCore.Functions.GetPlayer(source)
+	if not Player.Functions.GetItemByName(item.name) then return end
     TriggerClientEvent("megaphone:Toggle",source)
 end)
